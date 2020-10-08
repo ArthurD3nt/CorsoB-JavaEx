14b9bcbed74ad7c527955eae7f27635fb2c297e3
ArthurD3nt <francescomazzucco01@gmail.com>
2020-10-08 16:53:50 +0200

    added EmersioneMassimo.java

---
+++ Fiandrotti/002-lezione/EmersioneMassimo.java
@@ -0,0 +1,36 @@
+public class EmersioneMassimo {
+    public static void main (String[] args) {
+        //4 variabili randomizzate
+        int a = (int)(Math.random()*100);
+        int b = (int)(Math.random()*100);
+        int c = (int)(Math.random()*100);
+        int d = (int)(Math.random()*100);
+
+        int tmp; //variabile di appoggio 
+        int i = 0; //variabile per il calcolo del numero di iterazioni
+        while (!(a <= b && b <= c && c <= d)) {
+            if (a > b) {
+                tmp = a;
+                a = b;
+                b = tmp;
+            } if (b > c) {
+                tmp = b;
+                b = c;
+                c = tmp;
+            } if (c > d) {
+                tmp = d;
+                d = c;
+                c = tmp;
+            }
+            i++;
+        }
+        //Printa su cmdline
+        System.out.println("Variabili ordinate: ");
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("Con " + i + " iterazioni");
+
+    }
+}
