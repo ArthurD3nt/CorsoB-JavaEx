b8a5199623743e4daf31804070262debcf8aa0cb
ArthurD3nt <francescomazzucco01@gmail.com>
2020-12-10 16:27:37 +0000

    Esercizio pucca

---
+++ Esercizi/Esercizi Svolti/random/Ex003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Ex003 {
+  public static void main (String[] args) {
+
+    Scanner input = new Scanner (System.in);
+    int eta;
+    String nome;
+    System.out.println("Digita il tuo nome completo.");
+    nome = input.nextLine();
+    System.out.println("Digita la tua eta'. ");
+    eta = input.nextInt();
+    ;
+    System.out.println("Il tuo mome in maiuscolo e' "
+
+    +nome.toUpperCase() + " e hai " + eta + " anni.");
+      }
+}
